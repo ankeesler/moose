c8c6136cc3b50235fea567dd246e4c2cbfd6c0bc
Andrew Keesler <ankeesler1@gmail.com>
2018-03-03 01:43:54 -0500

    Add initial lazy.sh file.

---
+++ lazy.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+curl https://raw.githubusercontent.com/ankeesler/moose/master/install.sh | bash
+source ~/.profile
+curl https://raw.githubusercontent.com/ankeesler/moose/master/setup.sh | bash
