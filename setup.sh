2f7afee79b14ee7121036cf5c217c5266174aeff
Andrew Keesler <ankeesler1@gmail.com>
2018-03-04 00:21:05 +0000

    Install vim-go in setup.sh.

--- setup.sh
+++ setup.sh
@@ -8,5 +8,8 @@ git config --global user.email ankeesler1@gmail.com
 git config --global user.name "Andrew Keesler"
 git config --global push.default simple
 
+git clone https://github.com/fatih/vim-go.git ~/.vim/pack/plugins/start/vim-go
+
 go get -t github.com/ankeesler/anwork/cmd/anwork
 go install github.com/onsi/ginkgo/ginkgo
+
