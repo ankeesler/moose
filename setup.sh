7687d457c8baa599c48698e328aef42eda1f40e4
Andrew Keesler <ankeesler1@gmail.com>
2018-03-04 01:00:44 +0000

    Download moose and retro-rach repos upon setup.

--- setup.sh
+++ setup.sh
@@ -10,6 +10,9 @@ git config --global push.default simple
 
 git clone https://github.com/fatih/vim-go.git ~/.vim/pack/plugins/start/vim-go
 
+git clone https://github.com/ankeesler/moose.git ~/workspace/moose
+
 go get -t github.com/ankeesler/anwork/cmd/anwork
 go install github.com/onsi/ginkgo/ginkgo
 
+go get github.com/ankeesler/retro-rach
